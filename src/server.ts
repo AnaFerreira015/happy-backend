3999c7c22d89a757863637be331767f768c7e598
AnaFerreira015 <dasilvaferreira.af@hotmail.com>
2020-10-15 17:50:15 -0300

    Cria a rota inicial

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/users', (request, response) => {
     return response.json({ message: "Hello World" });
 })
